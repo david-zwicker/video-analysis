e7136a41edaab0f55356be6a07ab0850742a8d1d
David Zwicker <zwicker@pks.mpg.de>
2014-08-01 20:32:43 -0400

    Forgot to add the new files in the last commit. Here, we now have the filters and also two new packages, that will be used for the implementation of the video analysis

---
+++ video/filters.py
@@ -0,0 +1,38 @@
+'''
+Created on Aug 1, 2014
+
+@author: zwicker
+
+Filter are generators that take a video as an input and return filtered
+output as one iterates over them
+'''
+
+
+def crop(source, rect):
+    """ crops the video to the given rect=(left, top, right, bottom) """ 
+    for frame in source:
+        return frame[rect[0]:rect[2], rect[1]:rect[3], :]
+
+
+def difference(source):
+    """ returns the differences between consecutive frames """ 
+    
+    # get the iterator from the video
+    data = iter(source)
+    
+    # iterate through all frames and yield the difference
+    last_frame = data.next()
+    while data:
+        frame = data.next()
+        yield frame - last_frame
+        last_frame = frame
+        
+        
+def normalize_brightness(source):
+    """
+    adjusts individual frames such that their brightness corresponds to
+    the initial frame
+    """ 
+    raise NotImplementedError
+
+
