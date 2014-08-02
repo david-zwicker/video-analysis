bb86994c89529facb14b29698d4527916bd493df
David Zwicker <zwicker@pks.mpg.de>
2014-08-02 13:43:12 -0400

    Refactored the filters, such that some of the filters allow direct access of specific frames.

--- video/filters.py
+++ video/filters.py
@@ -4,7 +4,10 @@ Created on Aug 1, 2014
 @author: zwicker
 
 Filter are iterators that take a video as an input and return a special Video
-that can be iterated over, but that doesn't store its data in memory
+that can be iterated over, but that doesn't store its data in memory.
+
+Some filters allow to access the underlying frames at random using the get_frame method
+
 '''
 
 import logging
@@ -12,8 +15,7 @@ import numpy as np
 
 from formats.base import VideoBase
 
-
-class VideoIteratorBase(VideoBase):
+class VideoFilterBase(VideoBase):
     """ class which does not hold its own data, but is more like a view """
      
     def __init__(self, source, size=None, frame_count=None, fps=None, is_color=None):
@@ -27,22 +29,88 @@ class VideoIteratorBase(VideoBase):
         is_color = source.is_color if is_color is None else is_color
         
         # initialize the base video
-        super(VideoIteratorBase, self).__init__(
+        super(VideoFilterBase, self).__init__(
             size=size, frame_count=frame_count, fps=fps, is_color=is_color
         )
-    
-    def set_frame_pos(self, index):
-        raise ValueError('Iterators do not allow to seek a specific position')
-    
-    def __iter__(self):
-        return self
-    
-    def next(self):
+        
+    def _filter_frame(self, frame):
+        """ returns the frame with a filter applied """
         raise NotImplementedError
     
+    def __iter__(self):
+        self._source.set_frame_pos(0)
+        return self
+    
+    def get_frame(self, index):
+        return self._filter_frame(self._source.get_frame(index))
+                
+    def next(self):
+        return self._filter_frame(self._source.next())
+    
 
 
-class Crop(VideoIteratorBase):
+#===============================================================================
+# FILTERS THAT ALLOW SEEKING IN THE MATERIAL
+#===============================================================================
+
+
+class TimeSlice(VideoFilterBase):
+    """ iterates only over part of the frames """
+    
+    def __init__(self, source, start=0, stop=-1, step=1):
+        
+        # interpret negative indices as counting from the end of the video
+        if start < 0:
+            self._start = source.frame_count + start
+        else:
+            self._start = start
+            
+        if stop < 0:
+            self._stop = source.frame_count + stop
+        else:
+            self._stop = stop
+            
+        self._step = step
+            
+        # calculate the number of frames to be expected
+        frame_count = int(np.ceil((self._stop - self._start)/self._step)) 
+
+        # correct the size, since we are going to crop the movie
+        super(TimeSlice, self).__init__(source, frame_count=frame_count)
+
+        
+    def set_frame_pos(self, index):
+        frame_index = self._start + index*self._step
+        if not self._start <= frame_index < self._end:
+            raise IndexError 
+        self._source.set_frame_pos(frame_index)
+        
+        
+    def get_frame(self, index):
+        frame_index = self._start + index*self._step
+        if not self._start <= frame_index < self._end:
+            raise IndexError 
+        self._source.get_frame(frame_index)
+        
+        
+    def next(self):
+        if self._step == 1:
+            # check whether we are already beyond our end
+            if self.get_frame_pos() >= self._end:
+                raise StopIteration
+        else:
+            # advance to the next frame using step
+            try:
+                self.set_frame_pos(self._frame_pos)
+            except IndexError:
+                raise StopIteration
+
+        # return the next frame in question
+        return self._source.next()
+
+
+
+class Crop(VideoFilterBase):
     """ crops the video to the given rect=(left, top, right, bottom) """
     
     def __init__(self, source, rect):
@@ -60,22 +128,56 @@ class Crop(VideoIteratorBase):
         
         # correct the size, since we are going to crop the movie
         super(Crop, self).__init__(source, size=size)
-                
-    def next(self):
+       
+    def _filter_frame(self, frame):
         r = self.rect
-        return self._source.next()[r[0]:r[2], r[1]:r[3]]
+        return frame[r[0]:r[2], r[1]:r[3]]
 
 
 
-class TimeDifference(VideoIteratorBase):
-    """ returns the differences between consecutive frames """ 
+class Monochrome(VideoFilterBase):
+    """ returns the video as monochrome """
+    
+    def __init__(self, source, mode='normal'):
+        self.mode = mode.lower()
+        super(Monochrome, self).__init__(source, is_color=False)
+
+    def _filter_frame(self, frame):
+        if self.mode == 'normal':
+            return np.mean(frame, axis=2)
+        elif self.mode == 'r':
+            return frame[:, :, 0]
+        elif self.mode == 'g':
+            return frame[:, :, 1]
+        elif self.mode == 'b':
+            return frame[:, :, 2]
+        else:
+            raise ValueError('Unsupported conversion to monochrome: %s' % self.mode)
+    
+
+#===============================================================================
+# FILTERS THAT CAN ONLY BE USED AS ITERATORS
+#===============================================================================
+
+
+class TimeDifference(VideoFilterBase):
+    """
+    returns the differences between consecutive frames.
+    Here, frames cannot be accessed directly, but one can only iterate over the video
+    """ 
     
     def __init__(self, source):
         # correct the frame count since we are going to return differences
         super(TimeDifference, self).__init__(source, frame_count=source.frame_count-1)
         # store the first frame, because we always need a previous frame
         self.prev_frame = self._source.next()
-                
+    
+    def set_frame_pos(self, index):
+        raise ValueError('Iterators do not allow to seek a specific position')
+      
+    def get_frame(self, index):
+        raise ValueError('Iterators do not allow to seek a specific position')
+    
     def next(self):
         # get this frame and subtract from it the previous one
         this_frame = self._source.next()
@@ -87,24 +189,3 @@ class TimeDifference(VideoIteratorBase):
         return diff
   
         
-
-class Monochrome(VideoIteratorBase):
-    """ returns the video as monochrome """
-    
-    def __init__(self, source, mode='normal'):
-        self.mode = mode.lower()
-        super(Monochrome, self).__init__(source, is_color=False)
-
-    def next(self):
-        frame = self._source.next()
-        if self.mode == 'normal':
-            return np.mean(frame, axis=2)
-        elif self.mode == 'r':
-            return frame[:, :, 0]
-        elif self.mode == 'g':
-            return frame[:, :, 1]
-        elif self.mode == 'b':
-            return frame[:, :, 2]
-        else:
-            raise ValueError('Unsupported conversion to monochrome: %s' % self.mode)
-
