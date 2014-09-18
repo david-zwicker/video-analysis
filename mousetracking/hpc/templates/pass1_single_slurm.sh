04351daae1a3ca07f0176cd7100d49eba4bcb98b
David Zwicker <zwicker@pks.mpg.de>
2014-09-18 18:59:26 -0400

    Fixed bug in slurm batch files

--- mousetracking/hpc/templates/pass1_single_slurm.sh
+++ mousetracking/hpc/templates/pass1_single_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1                 # Ensure that all cores are on one machine
 #SBATCH -t {PASS1_TIME}      # Runtime in minutes
 #SBATCH -p {PARTITION}       # Partition to submit to
-#SBATCH ----mem-per-cpu={PASS1_MEMORY} # Memory per cpu in MB (see also --mem)
+#SBATCH --mem-per-cpu={PASS1_MEMORY} # Memory per cpu in MB (see also --mem)
 #SBATCH -o {JOB_DIRECTORY}/log_pass1_%j.txt    # File to which stdout and stderr will be written
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user={USER_EMAIL}
