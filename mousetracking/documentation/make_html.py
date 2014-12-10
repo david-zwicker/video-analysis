fa58f5c1d4667ccce3147a02b4ead535adf51be6
David Zwicker <dzwicker@seas.harvard.edu>
2014-12-10 15:55:51 -0500

    Added script that converts the markdown documentation into an html documentation

---
+++ mousetracking/documentation/make_html.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+'''
+Created on Dec 10, 2014
+
+@author: David Zwicker <dzwicker@seas.harvard.edu>
+'''
+
+
+from grip.command import main
+
+main(['statistics.md', '--export', 'html/statistics.html'])
