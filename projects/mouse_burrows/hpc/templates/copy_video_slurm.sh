735f42f1588f85014e28e74a16fb36f471085954
David Zwicker <dzwicker@seas.harvard.edu>
2015-10-02 18:38:20 -0400

    Increase memory limit

--- projects/mouse_burrows/hpc/templates/copy_video_slurm.sh
+++ projects/mouse_burrows/hpc/templates/copy_video_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1                 # Number of cores
 #SBATCH -t 24:00             # Runtime in minutes
 #SBATCH -p serial_requeue    # Partition to submit to
-#SBATCH --mem-per-cpu=500    # Memory per cpu in MB (see also --mem)
+#SBATCH --mem-per-cpu=2000   # Memory per cpu in MB (see also --mem)
 #SBATCH -o {JOB_DIRECTORY}/log_copy_video_%j.txt    # File to which stdout and stderr will be written
 #SBATCH --job-name=C_{NAME}
 #SBATCH --mail-type=FAIL
