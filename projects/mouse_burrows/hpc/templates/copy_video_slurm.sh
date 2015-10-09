6fba295313ddc22a2c4ebc0168e1bdca9cfc56c8
David Zwicker <dzwicker@seas.harvard.edu>
2015-10-09 14:01:14 -0400

    Added more control over the ressources of the copy pass

--- projects/mouse_burrows/hpc/templates/copy_video_slurm.sh
+++ projects/mouse_burrows/hpc/templates/copy_video_slurm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -n 1                 # Number of cores
-#SBATCH -t 24:00             # Runtime in minutes
+#SBATCH -n {PASS0/CORES}     # Number of cores
+#SBATCH -t {PASS0/TIME}      # Runtime in minutes
 #SBATCH -p serial_requeue    # Partition to submit to
-#SBATCH --mem-per-cpu=2000   # Memory per cpu in MB (see also --mem)
+#SBATCH --mem-per-cpu={PASS0/MEMORY}   # Memory per cpu in MB (see also --mem)
 #SBATCH -o {JOB_DIRECTORY}/log_copy_video_%j.txt    # File to which stdout and stderr will be written
 #SBATCH --job-name=C_{NAME}
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=dzwicker@seas.harvard.edu
+#SBATCH --mail-user={NOTIFICATION_EMAIL}
 
 echo "Start job with id $SLURM_JOB_ID"
 
